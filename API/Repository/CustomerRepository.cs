0f9b446c0d10ddd9a22c6aedca2b4a7cd0483f19
Alveen Chriskofasius <alveen.chriskofasius@gmail.com>
2024-02-17 17:26:13 +0700

    simplify function, change create by and update by using user identity name

--- API/Repository/CustomerRepository.cs
+++ API/Repository/CustomerRepository.cs
@@ -6,104 +6,105 @@ using System.Net;
 
 namespace API.Repository
 {
-	public class CustomerRepository : ICustomerRepository
-	{
-		private readonly MKSTableContext _context;
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly MKSTableContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CustomerRepository(MKSTableContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                _context.Customers.Remove(await _context.Customers.FindAsync(id));
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                return Task.FromResult<object>(new { success = false, error = e.Message });
+            }
+            return Task.FromResult<object>(new { success = true });
 
-		public CustomerRepository(MKSTableContext context)
-		{
-			_context = context;
-		}
-		public async Task<object> Delete(int id)
-		{
-			try
-			{
-				_context.Customers.Remove(await _context.Customers.FindAsync(id));
-				await _context.SaveChangesAsync();
-			}
-			catch (Exception e)
-			{
-				return Task.FromResult<object>(new { success = false, error = e.Message });
-			}
-			return Task.FromResult<object>(new { success = true });
+        }
 
-		}
+        public async Task<CustomerModel> FillForm(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            CustomerModel customerModel = null;
+            if (customer == null)
+            {
+                customerModel = new CustomerModel();
+            }
+            else
+            {
+                customerModel = new CustomerModel
+                {
+                    ID = id,
+                    Address = customer.Address,
+                    ContactNumber = customer.ContactNumber,
+                    ContactPerson = customer.ContactPerson,
+                    IsSupplier = customer.IsSupplier,
+                    Name = customer.Name,
+                    Note = customer.Note,
+                };
+            }
+            return customerModel;
+        }
 
-		public async Task<CustomerModel> FillForm(int id)
-		{
-			Customer customer = await _context.Customers.FindAsync(id);
-			CustomerModel customerModel = null;
-			if (customer == null)
-			{
-				customerModel = new CustomerModel();
-			}
-			else
-			{
-				customerModel = new CustomerModel
-				{
-					ID = id,
-					Address = customer.Address,
-					ContactNumber = customer.ContactNumber,
-					ContactPerson = customer.ContactPerson,
-					IsSupplier = customer.IsSupplier,
-					Name = customer.Name,
-					Note = customer.Note,
-				};
-			}
-			return customerModel;
-		}
+        public async Task<object> GetList()
+        {
+            return await _context.Customers.Select(x => new
+            {
+                ID = x.ID,
+                Name = x.Name,
+                ContactNumber = x.ContactNumber,
+                ContactPerson = x.IsSupplier ? x.ContactPerson : "-",
+                IsSupplier = x.IsSupplier ? "Supplier" : "Customer",
+            }).ToListAsync();
+        }
 
-		public async Task<object> GetList()
-		{
-			return await _context.Customers.Select(x => new
-			{
-				ID = x.ID,
-				Name = x.Name,
-				ContactNumber = x.ContactNumber,
-				ContactPerson = x.IsSupplier ? x.ContactPerson : "-",
-				IsSupplier = x.IsSupplier ? "Supplier" : "Customer",
-			}).ToListAsync();
-		}
+        public async Task<object> Save(CustomerModel customer)
+        {
+            try
+            {
+                if (customer.ID == 0)
+                {
+                    await _context.Customers.AddAsync(new Customer
+                    {
+                        Name = customer.Name,
+                        ContactPerson = customer.IsSupplier ? customer.ContactPerson : "-",
+                        ContactNumber = customer.ContactNumber,
+                        Address = customer.Address,
+                        IsSupplier = customer.IsSupplier,
+                        Note = customer.Note,
+                        CreatedBy = _httpContextAccessor.HttpContext.User.Identity.Name,
 
-		public async Task<object> Save(CustomerModel customer)
-		{
-			try
-			{
-				if (customer.ID == 0)
-				{
-					await _context.Customers.AddAsync(new Customer
-					{
-						Name = customer.Name,
-						ContactPerson = customer.IsSupplier ? customer.ContactPerson : "-",
-						ContactNumber = customer.ContactNumber,
-						Address = customer.Address,
-						IsSupplier = customer.IsSupplier,
-						Note = customer.Note,
-						CreatedBy = AuthRepository.CurrentUser.UserName,
+                    });
+                }
+                else
+                {
+                    Customer data = await _context.Customers.FindAsync(customer.ID);
+                    data.Name = customer.Name;
+                    data.ContactPerson = customer.IsSupplier ? customer.ContactPerson : "-";
+                    data.ContactNumber = customer.ContactNumber;
+                    data.Address = customer.Address;
+                    data.IsSupplier = customer.IsSupplier;
+                    data.Note = customer.Note;
+                    data.UpdatedAt = DateTime.Now;
+                    data.UpdatedBy = _httpContextAccessor.HttpContext.User.Identity.Name;
+                    _context.Update(data);
+                }
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                return Task.FromResult<object>(new { success = false, error = e.Message });
+            }
+            return Task.FromResult<object>(new { success = true });
+        }
 
-					});
-				}
-				else
-				{
-					Customer data = await _context.Customers.FindAsync(customer.ID);
-					data.Name = customer.Name;
-					data.ContactPerson = customer.IsSupplier ? customer.ContactPerson : "-";
-					data.ContactNumber = customer.ContactNumber;
-					data.Address = customer.Address;
-					data.IsSupplier = customer.IsSupplier;
-					data.Note = customer.Note;
-					data.UpdatedAt = DateTime.Now;
-					data.UpdatedBy = AuthRepository.CurrentUser.UserName;
-					_context.Update(data);
-				}
-				await _context.SaveChangesAsync();
-			}
-			catch (Exception e)
-			{
-				return Task.FromResult<object>(new { success = false, error = e.Message });
-			}
-			return Task.FromResult<object>(new { success = true });
-		}
-
-	}
+    }
 }
--- API/Repository/SupplierRepository.cs
+++ API/Repository/SupplierRepository.cs
@@ -5,71 +5,72 @@ using MitraKaryaSystem.Models;
 
 namespace API.Repository
 {
-	public class SupplierRepository : ISupplierRepository
-	{
-		private readonly MKSTableContext _context;
+    public class SupplierRepository : ISupplierRepository
+    {
+        private readonly MKSTableContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public SupplierRepository(MKSTableContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
 
-		public SupplierRepository(MKSTableContext context)
-		{
-			_context = context;
-		}
+        public async Task DeleteSupplier(int id)
+        {
+            _context.Customers.Remove(await _context.Customers.FindAsync(id));
+            await _context.SaveChangesAsync();
+        }
 
-		public async Task DeleteSupplier(int id)
-		{
-			_context.Customers.Remove(await _context.Customers.FindAsync(id));
-			await _context.SaveChangesAsync();
-		}
+        public async Task<SupplierModel> FillFormSupplier(int id)
+        {
+            Customer? supplier = await _context.Customers.FindAsync(id);
+            SupplierModel? supplierModel = null;
+            if (supplier == null)
+            {
+                supplierModel = new SupplierModel();
+            }
+            else
+            {
+                supplierModel = new SupplierModel
+                {
+                    ID = supplier.ID,
+                    SupplierName = supplier.Name,
+                    ContactPerson = supplier.ContactPerson,
+                    ContactNumber = supplier.ContactNumber
+                };
+            }
+            return supplierModel;
+        }
 
-		public async Task<SupplierModel> FillFormSupplier(int id)
-		{
-			Customer? supplier = await _context.Customers.FindAsync(id);
-			SupplierModel? supplierModel = null;
-			if (supplier == null)
-			{
-				supplierModel = new SupplierModel();
-			}
-			else
-			{
-				supplierModel = new SupplierModel
-				{
-					ID = supplier.ID,
-					SupplierName = supplier.Name,
-					ContactPerson = supplier.ContactPerson,
-					ContactNumber = supplier.ContactNumber
-				};
-			}
-			return supplierModel;
-		}
+        public async Task<object> GetSupplierList()
+        {
+            return Task.FromResult<object>(await _context.Customers.Select(x => new { x.ID, SupplierName = x.Name, x.ContactPerson, x.ContactNumber, x.IsSupplier }).Where(x => x.IsSupplier).ToListAsync());
+        }
 
-		public async Task<object> GetSupplierList()
-		{
-			return Task.FromResult<object>(await _context.Customers.Select(x => new { x.ID, SupplierName = x.Name, x.ContactPerson, x.ContactNumber, x.IsSupplier }).Where(x => x.IsSupplier).ToListAsync());
-		}
-
-		public async Task SaveSupplier(SupplierModel supplierModel)
-		{
-			if (supplierModel.ID == 0)
-			{
-				_context.Customers.Add(new Customer
-				{
-					Name = supplierModel.SupplierName,
-					ContactPerson = supplierModel.ContactPerson,
-					ContactNumber = supplierModel.ContactNumber,
-					CreatedBy = AuthRepository.CurrentUser.FullName,
-					IsSupplier = true
-				});
-			}
-			else
-			{
-				Customer supplier = await _context.Customers.FindAsync(supplierModel.ID);
-				supplier.Name = supplierModel.SupplierName;
-				supplier.ContactPerson = supplierModel.ContactPerson;
-				supplier.ContactNumber = supplierModel.ContactNumber;
-				supplier.UpdatedAt = DateTime.Now;
-				supplier.UpdatedBy = AuthRepository.CurrentUser.FullName;
-				_context.Customers.Update(supplier);
-			}
-			await _context.SaveChangesAsync();
-		}
-	}
+        public async Task SaveSupplier(SupplierModel supplierModel)
+        {
+            if (supplierModel.ID == 0)
+            {
+                _context.Customers.Add(new Customer
+                {
+                    Name = supplierModel.SupplierName,
+                    ContactPerson = supplierModel.ContactPerson,
+                    ContactNumber = supplierModel.ContactNumber,
+                    CreatedBy = _httpContextAccessor.HttpContext.User.Identity.Name,
+                    IsSupplier = true
+                });
+            }
+            else
+            {
+                Customer supplier = await _context.Customers.FindAsync(supplierModel.ID);
+                supplier.Name = supplierModel.SupplierName;
+                supplier.ContactPerson = supplierModel.ContactPerson;
+                supplier.ContactNumber = supplierModel.ContactNumber;
+                supplier.UpdatedAt = DateTime.Now;
+                supplier.UpdatedBy = _httpContextAccessor.HttpContext.User.Identity.Name;
+                _context.Customers.Update(supplier);
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
 }
