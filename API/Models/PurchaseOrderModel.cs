5425bb4f76ad156a51aec2e1cf4d020fd45ecd36
Alveen Chriskofasius <alveen.chriskofasius@gmail.com>
2024-02-21 09:26:57 +0700

    remove unused property, add list product model

--- API/Models/PurchaseOrderModel.cs
+++ API/Models/PurchaseOrderModel.cs
@@ -3,14 +3,12 @@
     public class PurchaseOrderModel
     {
         public int ID { get; set; }
-
-        public short SupplierID { get; set; }
+        public int TradeID { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
 
         public short? StatusID { get; set; }
         public string No { get; set; }
-        public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
+        public List<PurchaseOrderDetailModel> PurchaseOrderDetails { get; set; }
     }
 }
